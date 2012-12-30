ed024cd336c25c2927e3b4360f22d4e0d3a9b3e4
rocktronica <tommy@iamnotagoodartist.com>
2012-12-29 22:46:16 -0800

    Major JS refactoring

--- extension/extension.js
+++ extension/extension.js
@@ -1,34 +1,78 @@
 (function(undefined) {
-	var nav = document.querySelector("[data-element='nav']");
-	nav.on("click", "header", function(event){
-		event.preventDefault();
-		chrome.tabs.executeScript(null, {
-			file: this.dataset.filename
-		});
-	});
 
-	var sections = document.querySelectorAll("section");
-	location.hash = sections[0].id;
+	var bookmarklets = new Collection({
+		model: Model, // mehhhh
+		fetch: function() {
+			ajax({
+				url: "scripts.json",
+				callback: function(resp) {
+					var scripts = JSON.parse(resp);
+					this.reset();
+					scripts.forEach(function(script) {
+						script.filename = "scripts/" + script.filename;
+						this.push(new this.model(script));
+					}.bind(this));
+				}.bind(this)
+			});
+			return this;
+		}
+	}).fetch();
 
-	var template = {
-		nav: document.querySelector("[data-template='nav-item']").innerHTML
-	};
+	var bookmarkletsView = new View({
+		el: document.querySelector("[data-element='bookmarklets']"),
+		model: bookmarklets,
+		template:
+			document.querySelector("[data-template='nav-item']").innerHTML,
+		init: function() {
+			// could use some throttle here
+			this.model.on("change", this.update, this);
+			this.el.on("click", "header", this.clicked);
+		},
+		update: function() {
+			var htmlItems = [];
+			this.model.forEach(function(item) {
+				htmlItems.push(tmpl(this.template, item));
+			}.bind(this));
+			this.html(htmlItems);
+		},
+		clicked: function(event) {
+			chrome.tabs.executeScript(null, {
+				file: this.dataset.filename
+			});
+			event.preventDefault();
+		}
+	}).render();
 
-	ajax("scripts.json", function() {
-		var scripts = JSON.parse(this.response);
-		var html = [];
-		scripts.forEach(function(script) {
-			script.filename = "scripts/" + script.filename;
-			html.push(tmpl(template.nav, script));
-		});
-		nav.innerHTML += html.join("");
-	});
+	var app = new View({
+		el: "body",
+		render: function() {
+			this.sections = this.el.querySelectorAll("section[data-section]");
+			this.navLinks = this.el.querySelectorAll("a[href^='#']");
+			this.showSection(0);
+
+			this.navLinks.on("click", function(event) {
+				this.showSection(event.target.hash.substr(1));
+			}.bind(this));
+		},
+		showSection: function(which) {
+			this.sections.hide();
+			var sectionEl = (typeof which === "number") ?
+				this.sections[which] :
+				sectionEl = this.sections.filter("section[id='" + which + "']");
+			sectionEl.show();
+
+			which = sectionEl.id;
+
+			// Getting browser to recognize height sets container too
+			sectionEl.height();
+
+			this.navLinks.removeClass("active")
+				.filter("a[href='#" + which + "']").addClass("active");
+		}
+	}).render();
 
 	// Check for updates
 
 	// Sort by most recently used
-
-	// window.onerror
-	// and override console into a  pre
 
 }());
--- extension/setup.js
+++ extension/setup.js
@@ -1,30 +1,255 @@
-var ajax = function(url, callback) {
+// General Functions
+
+var noop = function() {};
+
+var ajax = function(options) {
 	var req = new XMLHttpRequest();
-	req.open("GET", chrome.extension.getURL(url), true)
-	req.onload = callback; // how to pass arg?
-	req.send(null);
+	options.callback = options.callback || noop;
+	options.method = (options.method || "GET").toUpperCase();
+	req.open(options.method, chrome.extension.getURL(options.url), true);
+	req.onload = function() {
+		options.callback.bind(req)(req.response);
+	};
+	req.send();
 };
 
-Node.prototype.on = function(eventName, targetSelector, callback) {
-	var parent = this;
-	this.addEventListener(eventName, function(event) {
-		if (event.target) {
-			var targets = parent.querySelectorAll(targetSelector) || [],
-				count = targets.length;
-			for (var i = 0; i < count; i++) {
-				var target = targets[i];
-				if (target === event.target || target.contains(event.target)) {
-					callback.bind(target)(event);
-					return;
-				}
-			}
-		}
-	});
-};
-
-// http://mir.aculo.us/2011/03/09/little-helpers-a-tweet-sized-javascript-templating-engine/
-function tmpl(s,d){
+var tmpl = function(s, d) { // via mir.aculo.us
 	for(var p in d)
 		s=s.replace(new RegExp('{{'+p+'}}','g'), d[p]);
 	return s;
 }
+
+var extend = function() {
+	return {}.extend(arguments);
+};
+
+Object.prototype.extend = function() {
+	arguments.forEach(function(object) {
+		if (typeof object === "object") {
+			for (key in object) {
+				if (object.hasOwnProperty(key)) {
+					this[key] = object[key];
+				}
+			}
+		}
+	}.bind(this));
+};
+
+Object.prototype.forEach = function(funct) {
+	for (key in this) {
+		if (this.hasOwnProperty(key)) {
+			funct.bind(this)(this[key]);
+		}
+	}
+};
+
+// DOM Manipulation
+
+Object.prototype.toArray = function() {
+	if (Array.isArray(this)) { return this; }
+	return Array.prototype.slice.call(this);
+};
+
+Node.prototype.nodes = NodeList.prototype.nodes = function() {
+	if (Array.isArray(this)) { return this; }
+	return (this.length) ? this.toArray() : [this];
+};
+
+Node.prototype.filter = NodeList.prototype.filter = function(selector) {
+	var outputNode;
+	this.nodes().forEach(function(node) {
+		if (node.webkitMatchesSelector(selector)) {
+			outputNode = node;
+		}
+	});
+	return outputNode;
+};
+
+Node.prototype.css = NodeList.prototype.css = function(rules) {
+	this.nodes().forEach(function(node) {
+		for (key in rules) {
+			node.style[key] = rules[key];
+		}
+	});
+	return this;
+};
+
+Node.prototype.addClass = NodeList.prototype.addClass = function(className) {
+	this.nodes().forEach(function(node) {
+		node.classList.add(className);
+	});
+	return this;
+};
+
+Node.prototype.removeClass = NodeList.prototype.removeClass = function(className) {
+	this.nodes().forEach(function(node) {
+		node.classList.remove(className);
+	});
+	return this;
+};
+
+Node.prototype.height = NodeList.prototype.height = function(setTo) {
+	var node = this.nodes().slice(0,1)[0];
+	if (setTo) {
+		node.css("height", setTo);
+	}
+	return setTo || node.clientHeight;
+};
+
+Node.prototype.toggle = NodeList.prototype.toggle = function(show) {
+	this.nodes().forEach(function(node) {
+		// rename these variables
+		var makeVisible = (show !== undefined) ?
+			show : node.style.display === "hidden"
+		node.style.display = makeVisible ?
+			"block" : "none";
+	});
+	return this;
+};
+
+Node.prototype.show = NodeList.prototype.show = function() {
+	this.nodes().forEach(function(node) {
+		node.toggle(true);
+	});
+	return this;
+};
+
+Node.prototype.hide = NodeList.prototype.hide = function() {
+	this.nodes().forEach(function(node) {
+		node.toggle(false);
+	});
+	return this;
+};
+
+Node.prototype.on = NodeList.prototype.on = function(eventName, targetSelector, callback) {
+	this.nodes().forEach(function(node) {
+		node.addEventListener(eventName, function(event) {
+			if (event.target) {
+				var targets = [];
+				if (typeof targetSelector === "function") {
+					targets = [node];
+					callback = targetSelector;
+				} else {
+					targets = node.querySelectorAll(targetSelector).toArray();
+				}
+				targets.forEach(function(target) {
+					if (target === event.target || target.contains(event.target)) {
+						callback.bind(target)(event);
+						return;
+					}
+				});
+			}
+		});
+	});
+	return this;
+};
+
+// Models
+
+var Model = (function() {
+	var model = function(options) {
+		this.extend({
+			callbacks: {}
+		}, options);
+	};
+
+	model.fn = model.prototype;
+
+	model.fn.toString = model.fn.toJson = function() {
+		return JSON.stringify(this);
+	};
+
+	model.fn.trigger = function(event) {
+		if (this.callbacks[event]) {
+			for (var i = 0; i < this.callbacks[event].length; i++) {
+				var callback = this.callbacks[event][i];
+				callback.action.apply(callback.context);
+			}
+		}
+		return this;
+	};
+
+	model.fn.on = function(event, callback, that) {
+		this.callbacks[event] = this.callbacks[event] || [];
+		this.callbacks[event].push({
+			action: callback,
+			context: that || this
+		});
+		return this;
+	};
+
+	model.fn.set = function(key, value) {
+		this[key] = value;
+		this.trigger("set").trigger("set:" + key);
+		return this;
+	};
+
+	model.fn.get = function(key) {
+		this.trigger("get").trigger("get:" + key);
+		return this[key];
+	};
+
+	return model;
+}());
+
+// Collections
+
+var Collection = (function() {
+	var collection = function(options) {
+		this.extend({
+			items: []
+		}, options);
+	};
+
+	collection.fn = collection.prototype = new Model();
+
+	collection.fn.reset = function() {
+		this.items.length = 0;
+		this.trigger("reset").trigger("change");
+	};
+
+	collection.fn.push = function(item) {
+		this.items.push(item);
+		this.trigger("push").trigger("change");
+		return this;
+	};
+
+	collection.fn.forEach = function(callback) {
+		this.items.forEach(callback);
+	};
+
+	return collection;
+}());
+
+// Views
+
+var View = (function() {
+	var view = function(options) {
+		this.tagName = options.tagName || "div";
+
+		this.extend({
+			events: {},
+			el: document.createElement(this.tagName)
+		}, options);
+
+		if (typeof this.el === "string") {
+			this.el = document.querySelector(this.el);
+		}
+
+		this.init();
+	};
+
+	view.fn = view.prototype;
+
+	view.fn.init = view.fn.render = view.fn.update = function() {
+		return this;
+	};
+
+	view.fn.html = function(html) {
+		if (Array.isArray(html)) { html = html.join(""); }
+		this.el.innerHTML = html;
+		return this;
+	};
+
+	return view;
+}());
